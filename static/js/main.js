675bea369efb1c48ea9ec92eed160c3f885e96ab
SHanmukha-Y <shanmukha.yenneti@gmail.com>
2022-05-09 21:49:34 -0400

    Minor edits, removed barchart

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ window.onload = function() {
     geomap("Heart");
     getDataForState("New York");
     donut("New York");
-    barChart("New York");
+    // barChart("New York");
     betterbubbleChart("Heart");
     pcp();
 };
